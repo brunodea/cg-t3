d5fd046610469123d33db4d47c43a873954026cd
brunodea <brunodea@inf.ufsm.br>
2011-05-28 14:23:56 -0300

    GL_DEPTH_TEST e GL_DEPTH_BUFFER_BIT adicionados ao render do Canvas.

--- src/GUI/Canvas/Canvas.cpp
+++ src/GUI/Canvas/Canvas.cpp
@@ -12,6 +12,10 @@ void Canvas::update()
 
 void Canvas::render()
 {
+    glEnable(GL_DEPTH_TEST);
+
     glClearColor(0.f, 0.f, 0.f, 1.f);
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glDisable(GL_DEPTH_TEST);
 }
